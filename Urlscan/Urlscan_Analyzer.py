3a824cd42c961e59c78944c6a78a16f12470a8af
Maria <maria.gvaliente@alumnos.upm.es>
2020-04-16 14:06:59 +0200

    Fix some things

--- Urlscan/Urlscan_Analyzer.py
+++ Urlscan/Urlscan_Analyzer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 from cortexutils.analyzer import Analyzer
 import requests
+import datetime
+from dateutil.parser import parse
 
 class UrlscanAnalyzer(Analyzer):
     def __init__(self):
@@ -10,33 +12,24 @@ class UrlscanAnalyzer(Analyzer):
         self.basic_url = "https://urlscan.io/api/v1/result/"
 
     def get_scan(self):
-    	data = self.getData()
-    	indicator_value = str(data)
-		response = requests.get('https://urlscan.io/api/v1/search?q=domain:{}'.format(indicator_value),headers=self.headers)
-		if response.status_code == 200:
+        data = self.getData()
+        indicator_value = str(data)
+        response = requests.get('https://urlscan.io/api/v1/search?q=domain:{}'.format(indicator_value),headers=self.headers)
+        if response.status_code == 200:
             response_json = response.json()
-			response_results = response_json.get('results')
-			#ID Scan
-			id_url = response_results[0].get('_id')
-			return id_url
+            response_results = response_json.get('results')
+            #ID Scan
+            id_url = response_results[0].get('_id')
+            return id_url
         else:
             self.error("urlscan.io returns %s" % response.status_code)
 
     def get_results(self):
-    	id_url = self.get_scan()
-    	url_api_search = str(self.basic_url)
-    	response_search = requests.get(url_api_search + str(id_url), headers=self.headers)
-		res_search_json = response_search.json()
-		return res_search_json
-
-	def get_date(self, report):
-		data = report.get('data')
-		requests = data.get('requests')
-		response = requests[0].get('response')
-		response_final = response.get('response')
-		headers = response_final.get('headers')
-		date = headers.get('Date')
-		return date
+        id_url = self.get_scan()
+        url_api_search = str(self.basic_url)
+        response_search = requests.get(url_api_search + str(id_url), headers=self.headers)
+        res_search_json = response_search.json()
+        return res_search_json
 
     def summary(self, raw):
         taxonomies = []
@@ -45,23 +38,38 @@ class UrlscanAnalyzer(Analyzer):
 
         malicious = raw["verdicts"]["overall"]["malicious"]
         score = raw["verdicts"]["overall"]["score"]
-        tags = raw["verdicts"]["overall"]["score"]
-        last_seen = self.get_date()
+        tags = raw["verdicts"]["overall"]["tags"]
+        dates = []
+        data = raw.get('data')
+        requests = data.get('requests')
+        for request in requests:
+            response = request.get('response')
+            response_final = response.get('response')
+            headers = response_final.get('headers')
+            if headers != None:
+               last_seen = headers.get('Last-Modified')
+               if last_seen != None:
+                  last_seen_parsed = parse(last_seen).isoformat()
+                  dates.append(last_seen_parsed)
+            else:
+               date = "Not found"
+        if len(dates) != 0:
+           dates_sort = sorted(dates)
+           date = dates_sort[-1]
+        else:
+           date = "Not found"
 
         if malicious:
-            level = 'malicious'
+           level = 'malicious'
         elif score > 0:
-            level = 'suspicious'
+           level = 'suspicious'
         if score == None:
-        	score == "Not found"
-       	if len(tags) != 0:
-        	for tag in tags:
-        		taxonomies.append(self.build_taxonomy(level, namespace, "Tag", tag))
-
+           score == "Not found"
+        if len(tags) != 0:
+           for tag in tags:
+               taxonomies.append(self.build_taxonomy(level, namespace, "Tag", tag))
         taxonomies.append(self.build_taxonomy(level, namespace, "Score", score))
-        if last_seen != None:
-        	taxonomies.append(self.build_taxonomy(level, namespace, "Last_seen", last_seen))
-
+        taxonomies.append(self.build_taxonomy(level, namespace, "Last_seen", date))
         return {"taxonomies": taxonomies}
 
 
